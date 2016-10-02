aecd876a00f2e3574068b682bbc9f5c802dc93a0
Vishal Rao <vishalrao2911@gmail.com>
2016-10-02 19:13:55 -0400

    changing directory

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // configure a public directory to host static content
-app.use(express.static(__dirname + '/public/assignment/pages/'));
+app.use(express.static(__dirname + '/public'));
 
 require ("./test/app.js")(app);
 
