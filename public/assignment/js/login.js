90a46aec050ad187f616a49268056f31fdd76c48
Vishal Rao <vishalrao2911@gmail.com>
2016-10-02 11:46:12 -0400

    adding materialize and initial pages

---
+++ public/assignment/js/login.js
@@ -0,0 +1,7 @@
+/**
+ * Created by vishalrao on 9/29/16.
+ */
+// Include app dependency on ngMaterial
+
+var app = angular.module( 'webcreator', [ 'ngMaterial' ] )
+    .controller("LoginController", LoginController );
